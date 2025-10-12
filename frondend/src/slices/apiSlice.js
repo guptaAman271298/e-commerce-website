54c3b2f43f46f83caa24566dbca794a8537d32a2
Aman Gupta <gupta.aman27121998@gmail.com>
2025-10-12 19:32:26 +0530

    Checking env

--- frondend/src/slices/apiSlice.js
+++ frondend/src/slices/apiSlice.js
@@ -2,6 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({ baseUrl: import.meta.env.VITE_PRODUCT_API })
 
+console.log("API URL:", import.meta.env.VITE_PRODUCT_API);
+
 export const apiSlice = createApi({
     baseQuery,
     tagTypes: ['Product', 'Order', 'User'],
